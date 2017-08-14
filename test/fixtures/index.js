3ec5a56b541bbfbb195ccc5ec49ae8cb0209f8e1
Erica Pramer <epramer@godaddy.com>
2017-08-14 10:14:28 -0700

    fix shadowing of `err`

--- test/fixtures/index.js
+++ test/fixtures/index.js
@@ -1,27 +1,27 @@
 /* jscs: disable */
 exports.packageStrip = {
-  before: { _id: 'my-package',
-  name: 'my-package',
-  description: 'A kind of package',
-  'dist-tags': { latest: '1.0.0' },
-  versions:
+  before: { '_id': 'my-package',
+    'name': 'my-package',
+    'description': 'A kind of package',
+    'dist-tags': { latest: '1.0.0' },
+    'versions':
    { '1.0.0':
       { name: 'my-package',
         config: { locales: ['en-US'] },
         description: 'A kind of package',
         dependencies: { 'are-only-in-version': '*' },
         webpack: {},
-        main: 'index.js' } },
-  readme: 'ERROR: No README data found!',
-  maintainers: [ { name: 'indexzero', email: 'charlie.robbins@gmail.com' } ],
-  _attachments: {} },
+        main: 'index.js' }},
+    'readme': 'ERROR: No README data found!',
+    'maintainers': [{ name: 'indexzero', email: 'charlie.robbins@gmail.com' }],
+    '_attachments': {}},
   after: { name: 'my-package',
-  config: { locales: '["en-US"]' },
-  description: 'A kind of package',
-  dependencies: { 'are-only-in-version': '*' },
-  main: 'index.js',
-  distTags: { latest: '1.0.0' },
-  extended: JSON.parse('{"webpack": {}, "maintainers":[{"name":"indexzero","email":"charlie.robbins@gmail.com"}]}') }
+    config: { locales: '["en-US"]' },
+    description: 'A kind of package',
+    dependencies: { 'are-only-in-version': '*' },
+    main: 'index.js',
+    distTags: { latest: '1.0.0' },
+    extended: JSON.parse('{"webpack": {}, "maintainers":[{"name":"indexzero","email":"charlie.robbins@gmail.com"}]}') }
 };
 
 exports.buildFile = {
