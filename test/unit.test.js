d120634482f81bac32017253c311888d4f3f92d5
wjarvis <wjarvis@godaddy.com>
2019-10-30 12:02:18 -0700

    [dynamo test] Updated unit tests to use new dynamo format. removed fetchFiles and deserialize functions from build, build-file, and build-head.

--- test/unit.test.js
+++ test/unit.test.js
@@ -3,48 +3,34 @@
 var assume = require('assume');
 var clone = require('clone');
 var models = require('..');
-var mocks = require('datastar-test-tools').mocks;
-var helpers = require('datastar-test-tools').helpers;
+var dynamo = require('dynamodb');
+var Dynastar = require('dynastar');
 var fixtures = require('./fixtures');
 
 var packageStrip = fixtures.packageStrip;
-var buildFileFix = fixtures.buildFile;
 
 describe('warehouse-models (unit)', function () {
   var dal;
   beforeEach(function () {
-    var datastar = helpers.connectDatastar({ mock: true }, mocks.datastar());
-    dal = models(datastar);
+    dal = models(dynamo);
   });
 
   it('should return the appropriate object', function () {
-    assume(dal.Build).is.a('function');
-    assume(dal.BuildFile).is.a('function');
-    assume(dal.BuildHead).is.a('function');
-    assume(dal.Package).is.a('function');
-    assume(dal.PackageCache).is.a('function');
-    assume(dal.Version).is.a('function');
-    assume(dal.Dependent).is.a('function');
+    assume(dal.Build).is.a(Dynastar);
+    assume(dal.BuildFile).is.a(Dynastar);
+    assume(dal.BuildHead).is.a(Dynastar);
+    assume(dal.Package).is.a(Dynastar);
+    assume(dal.PackageCache).is.a(Dynastar);
+    assume(dal.Version).is.a(Dynastar);
+    assume(dal.Dependent).is.a(Dynastar);
   });
 
   it('should have an `ensure` function', function () {
     assume(dal.ensure).is.a('function');
   });
 
-  it('should contain a deserialize function on the BuildFile model that turns Buffers -> strings', function () {
-    assume(dal.BuildFile.deserialize).is.a('function');
-    var first = dal.BuildFile.deserialize(buildFileFix.array);
-    assume(first).is.an('array');
-    assume(first[0].source).is.a('string');
-    var second = dal.BuildFile.deserialize(buildFileFix.object);
-    assume(second).is.an('object');
-    assume(second.source).is.a('string');
-    assume(second.sourcemap).is.a('string');
-  });
-
-  it('should return a dal whos Build and BuildHead prototype has a fetchFiles function', function () {
-    assume(dal.Build.prototype.fetchFiles).is.a('function');
-    assume(dal.BuildHead.prototype.fetchFiles).is.a('function');
+  it('should have an `drop` function', function () {
+    assume(dal.drop).is.a('function');
   });
 
   it('should contain a defined function on the Package model that should strip values', function () {
